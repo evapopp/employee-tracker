0ae37713b7bcbbad50fc0dc079371321b79db2aa
eva <83175995+evapopp@users.noreply.github.com>
2021-10-04 14:07:37 -0700

    updated .env file to connect to server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+require('dotenv').config();
 
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      user: 'root',
-      password: 'EatPussyNotCats8',
-      database: 'employee_db'
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     },
     console.log(`Connected to the employee_db database.`)
 );
